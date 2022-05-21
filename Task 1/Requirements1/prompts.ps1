dfdb17d38298e2a12bf486fa882c17eecf506c64
Joe Seymour <poindexter12@gmail.com>
2022-05-21 09:22:48 -0700

    added default case

--- Task 1/Requirements1/prompts.ps1
+++ Task 1/Requirements1/prompts.ps1
@@ -23,29 +23,32 @@ Function Main () {
     Write-Host "Welcome to the WGU C916 Task 1 Script!"
 
     do {
-        Write-Host "*********************************************"
+        Write-Host "*********************************************************"
         Write-Host "Press 1 to append *.log files to DailyLog.txt"
         Write-Host "Press 2 to write files to C916contents.txt"
-        Write-Host "Press 3 to write files to C916contents.txt"
-        Write-Host "Press 4 to write files to C916contents.txt"
+        Write-Host "Press 3 to write files to list memory and cpu usage"
+        Write-Host "Press 4 to write files to list processes by virtual size"
         Write-Host "Press 5 to exit!"
-        Write-Host "*********************************************"
+        Write-Host "*********************************************************"
         Write-Host
         Write-Host "Please provide your input here [1-5]: "
 
-        $KeyInfo = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
-        Write-Host "KeyPress received: $KeyInfo"
-
+        $KeyInfo = $Host.UI.RawUI.ReadKey("IncludeKeyDown")
+        Write-Debug "KeyPress received: $KeyInfo"
+        Write-Host
+        
         Switch ($KeyInfo.Character) {
             '1' { AppendLogFiles }
             '2' { WriteFiles }
             '3' { ListMemoryAndCpuUsage }
             '4' { ListProcessesByVirtualSize }
             '5' { Exit 0 }
-            else {
+            default {
                 Write-Host "Unknown option selected, please try again"
             }
-        }    
+        }
+
+        Write-Host
     }
     until($KeyInfo.Character -Eq '5')
  }
